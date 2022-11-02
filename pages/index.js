eb4ddfe6b8c13336edb18617956fcdab9862f247
unknown <Sai1Ram>
2022-11-03 02:32:53 +0530

    Gallery

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
+import Gallery from "../components/gallery/Gallery";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+      <Gallery />
+    </>
+  );
 }

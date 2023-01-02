7cea3f1a0b1f1d241731d32c86caec7e5ab2b27b
aayush3135 <aayushjha3135@gmail.com>
2023-01-03 01:03:23 +0530

    merchpg

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
+import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import picture from '../assets/images/img1.jpeg'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <merch/>
   )
 }

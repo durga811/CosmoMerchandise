f4865e0817429fc86e79ad298433f152f3b7f7b2
Priyanshu Kumar Panda <priyanshupanda.ctp@gmail.com>
2023-01-04 13:53:52 +0530

    Revert "Merge pull request #9 from Cosmopolitan-2022/Anish"
    This reverts commit afcef74b8fcb060cd284f29898ff4a3a268ab6a6, reversing
    changes made to 80a84b2d404eda4549131bda0428d250c6a1f44d.

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=''>Footer</div>
+    <div>Footer</div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,11 @@
-// library
-import React from "react";
-import Image from "next/dist/client/image";
 
-// Widgets
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Button from "../components/Button";
-
-// Components
-import UpperSection from "../components/homePage/UpperSection";
-import Carousel from "../components/homePage/Carousel";
-import Highlights from "../components/homePage/Highlights";
-import Trending from "../components/homePage/Trending";
-import Events from "../components/homePage/Events";
-
-// ImageComponents
-import bgImage from "../assets/images/background.png";
-
-// Others
-import Homesection from "./homeSection";
+import Head from 'next/head'
+import Image from 'next/image'
+import Aboutus from '../components/Aboutus'
 
 export default function Home() {
   return (
-    <>
-      <div
-        className=" bg-[#391F26] bg-cover "
-        style={{ backgroundImage: `url(${bgImage.src})` }}
-      >
-        <div>
-          <Header />
-        </div>
-        <div className="body">
-          <UpperSection />
-          <div className="flex justify-center items-center">
-            <Button text={"DRAMA"} />
-            <Button text={"DANCE"} />
-            <Button text={"MUSIC"} />
-          </div>
-          <div className="m-[40px] flex justify-center items-center">
-            <Carousel />
-          </div>
-          <div className="flex justify-center items-center text-[#DCDCDC] font-[600] text-[48px] leading-[50px]">
-            HIGHLIGHTS
-          </div>
-          <div className="m-[40px] flex justify-center items-center">
-            <Highlights />
-          </div>
-          <div className="flex justify-center items-center text-[#DCDCDC] font-[600] text-[48px] leading-[50px]">
-            TRENDING VIDEOS
-          </div>
-          <div className="m-[40px] flex justify-center items-center">
-            <Trending />
-          </div>
-          <div className="">
-            <Events />
-          </div>
-          <div className="grid justify-center ">
-            <Footer />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    
+        <Aboutus/>
+  )
 }

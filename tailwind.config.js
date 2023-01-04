8f3b7046800d008e831b0154d3c010801c55129a
Priyanshu Kumar Panda <priyanshupanda.ctp@gmail.com>
2023-01-04 13:53:58 +0530

    Revert "Revert "Merge pull request #9 from Cosmopolitan-2022/Anish""
    This reverts commit f4865e0817429fc86e79ad298433f152f3b7f7b2.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,24 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      keyframes:{
-        textAnimation:{
-          "0%":{top:"430px"},
-          "50%":{top:"155px", fontSize:"380px", lineHeight:"341px"},
-          "100%":{top:"74px", fontSize:"96px", lineHeight:"117px"}
-        },
-        logo:{
-          "0%":{
-            opacity:"0"
-          },
-          "50%":{opacity:"0.5"},
-          "100%":{
-            opacity:"1"
-          }
-        }
-      }
-    },
+    extend: {},
   },
-  plugins: [require('tailwind-scrollbar-hide')],
+  plugins: [],
 }

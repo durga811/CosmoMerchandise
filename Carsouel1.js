7539ee1317fd02acbf85a36e78ece20110d8740d
PraTEEkgUpTa2 <97375784+PraTEEkgUpTa2@users.noreply.github.com>
2023-01-05 23:38:11 -0800

    Add files via upload

---
+++ Carsouel1.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Carsouel.css";
+
+function Carsouel1(){
+    return(
+        <div>
+           <section className="slider1">
+            <img className="image" id="img1" src="https://wallpaperaccess.com/full/2512452.jpg"  />
+            <img className="image" id="img2" src="https://wallpaperaccess.com/full/2512452.jpg"  />
+            <img  className="image" id="img3" src="https://wallpaperaccess.com/full/2512452.jpg"  />
+           </section>
+        </div>
+    )
+}
+
+export default Carsouel1;
---
+++ Footer1.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer1(){
+    return (
+        <div className="bottom-content">
+        <div className="content-left">
+            <div className="subcontent">
+               <h2>ABOUT</h2>
+               <p>Lorem ipsum dolor sit amet</p>
+            </div>
+        </div>
+        <div className="content-middle">
+            <div className="subcontent">
+                <h2>QUICK LINES</h2>
+                <ul>
+               <li><h3>About Us</h3></li>
+               <li><h3>Contact Us</h3></li>
+               <li><h3>Events</h3></li>
+               <li><h3>Register</h3></li>
+                </ul>
+            </div>
+            </div>
+            <div className="content-right">
+            <div className="subcontent">
+                <h2>SUPPORT</h2>
+                <ul>
+                <li><h3>Privacy Policy</h3></li>
+                <li><h3>Terms to Service</h3></li>
+                <li><h3>Legality</h3></li>
+                </ul>
+            </div>
+        </div>
+        <div className='bottom-image'>
+        <img id="btm-img1" src="https://i.ibb.co/k12yTpb/svg-1.png" border="0" />
+        <img id="btm-img2" src="https://i.ibb.co/p4rC7BF/svg-2-1.png"  border="0" />
+        <img id="btm-img3"  src="https://i.ibb.co/GWZ4SCy/svg-1-1.png" border="0" />
+        </div>
+    </div>
+    );
+}
+
+
+
+export default Footer1;
